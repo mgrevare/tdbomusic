fec3753b9ae982b51ff60dacdea95725eb4c8450
Mitch Revare <mgrevare@gmail.com>
2017-02-19 12:38:22 -0700

    connect page added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-$( ".about-page-content, .music-page-content, connect-page-content" ).hide();
+$( ".about-page-content, .music-page-content, .connect-page-content" ).hide();
 
 
 function handleAboutClick() {
   $(".js-about").click(function(){
-    $(".home-page-content").fadeOut("fast", function() {
+    $(".home-page-content, .connect-page-content").fadeOut("fast", function() {
       $(".about-page-content").fadeIn("fast");
     })
   });
@@ -11,12 +11,20 @@ function handleAboutClick() {
 
 function handleHomeClick() {
   $(".js-home").click(function(){
-    $(".about-page-content").fadeOut("fast", function() {
+    $(".about-page-content, .connect-page-content").fadeOut("fast", function() {
       $(".home-page-content").fadeIn("fast");
     })
 });
 };
 
-handleAboutClick();
+function handleConnectClick() {
+  $(".js-connect").click(function(){
+    $(".home-page-content, .about-page-content").fadeOut("fast", function() {
+      $(".connect-page-content").fadeIn("fast");
+    })
+  });
+};
 
+handleAboutClick();
+handleConnectClick();
 handleHomeClick();
