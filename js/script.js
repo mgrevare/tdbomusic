dddacd1e8687b726179c7981245ba19372b9d528
Mitch Revare <mgrevare@gmail.com>
2017-02-26 16:10:32 -0700

    logos

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $( ".about-page-content, .music-page-content, .connect-page-content" ).hide();
 function handleAboutClick() {
   $(".js-about").click(function(){
     $(".home-page-content, .connect-page-content").fadeOut("fast", function() {
-      $(".about-page-content").fadeIn("fast");
+      $(".about-page-content").fadeIn("slow");
     })
   });
 };
@@ -12,7 +12,7 @@ function handleAboutClick() {
 function handleHomeClick() {
   $(".js-home").click(function(){
     $(".about-page-content, .connect-page-content").fadeOut("fast", function() {
-      $(".home-page-content").fadeIn("fast");
+      $(".home-page-content").fadeIn("slow");
     })
 });
 };
@@ -20,7 +20,7 @@ function handleHomeClick() {
 function handleConnectClick() {
   $(".js-connect").click(function(){
     $(".home-page-content, .about-page-content").fadeOut("fast", function() {
-      $(".connect-page-content").fadeIn("fast");
+      $(".connect-page-content").fadeIn("slow");
     })
   });
 };
