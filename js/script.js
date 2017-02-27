1bb93cee87137bec50729fb12e0f43031e1b65f5
Mitch Revare <mgrevare@gmail.com>
2017-02-26 22:52:33 -0700

    jquery fixes

--- js/script.js
+++ js/script.js
@@ -3,28 +3,25 @@ $( ".about-page-content, .music-page-content, .connect-page-content" ).hide();
 
 function handleAboutClick() {
   $(".js-about").click(function(){
-    $(".home-page-content, .connect-page-content").fadeOut("fast", function() {
-      $(".about-page-content").fadeIn("slow");
-    })
+    $(".home-page-content, .connect-page-content").fadeOut("fast")
+    $(".about-page-content").fadeIn("fast");
   });
 };
 
 function handleHomeClick() {
   $(".js-home").click(function(){
-    $(".about-page-content, .connect-page-content").fadeOut("fast", function() {
-      $(".home-page-content").fadeIn("slow");
-    })
+    $(".about-page-content, .connect-page-content").fadeOut("fast");
+    $(".home-page-content").fadeIn("fast");
 });
 };
 
 function handleConnectClick() {
   $(".js-connect").click(function(){
-    $(".home-page-content, .about-page-content").fadeOut("fast", function() {
-      $(".connect-page-content").fadeIn("slow");
-    })
+    $(".home-page-content, .about-page-content").fadeOut("fast");
+    $(".connect-page-content").fadeIn("fast");
   });
 };
 
-handleAboutClick();
-handleConnectClick();
-handleHomeClick();
+$(handleAboutClick());
+$(handleConnectClick());
+$(handleHomeClick());
