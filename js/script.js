8a9566742b42e33684ea2d616afc64d010d64872
Mitch Revare <mgrevare@gmail.com>
2017-03-01 21:55:43 -0700

    faster

--- js/script.js
+++ js/script.js
@@ -3,21 +3,21 @@ $( ".about-page-content, .music-page-content, .connect-page-content" ).hide();
 
 function handleAboutClick() {
   $(".js-about").click(function(){
-    $(".home-page-content, .connect-page-content").fadeOut("fast")
+    $(".home-page-content, .connect-page-content").hide()
     $(".about-page-content").fadeIn("fast");
   });
 };
 
 function handleHomeClick() {
   $(".js-home").click(function(){
-    $(".about-page-content, .connect-page-content").fadeOut("fast");
+    $(".about-page-content, .connect-page-content").hide();
     $(".home-page-content").fadeIn("fast");
 });
 };
 
 function handleConnectClick() {
   $(".js-connect").click(function(){
-    $(".home-page-content, .about-page-content").fadeOut("fast");
+    $(".home-page-content, .about-page-content").hide();
     $(".connect-page-content").fadeIn("fast");
   });
 };
