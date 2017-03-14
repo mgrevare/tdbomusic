bfba6530ef6d2788f7651f54060dc181879b8447
Mitch Revare <mgrevare@gmail.com>
2017-03-13 22:40:00 -0600

    ready to go

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-// $( ".about-page-content, .music-page-content, .connect-page-content" ).hide();
-
+$(function(){
 
 function handleAboutClick() {
   $(".js-about").click(function(){
-    $(".home-page-content, .connect-page-content").addClass("hidden");
+    $(".home-page-content, .connect-page-content, music-page-content").addClass("hidden");
     $(".about-page-content").removeClass("hidden");
   });
 };
 
 function handleHomeClick() {
   $(".js-home").click(function(){
-    $(".connect-page-content, .about-page-content").addClass("hidden");
+    $(".connect-page-content, .about-page-content, .music-page-content").addClass("hidden");
     $(".home-page-content").removeClass("hidden");
     $(".mdl-layout__drawer").removeClass("is-visibe").attr("aria", "true");
 });
@@ -18,11 +17,23 @@ function handleHomeClick() {
 
 function handleConnectClick() {
   $(".js-connect").click(function(){
-    $(".home-page-content, .about-page-content").addClass("hidden");
+    $(".home-page-content, .about-page-content, .music-page-content").addClass("hidden");
     $(".connect-page-content").removeClass("hidden");
   });
 };
 
+function handleMusicClick() {
+  $(".js-music").click(function(){
+    $(".home-page-content, .about-page-content, .connect-page-content").addClass("hidden");
+    $(".music-page-content").removeClass("hidden");
+  });
+};
+
+$body = $("body");
+
 $(handleAboutClick());
 $(handleConnectClick());
 $(handleHomeClick());
+$(handleMusicClick());
+
+});
